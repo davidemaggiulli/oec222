c67a61ed5b2115e1d36fa8a3c1e8f9d3cd161e0c
Davide Maggiulli <davide.maggiulli@gmail.com>
2022-11-21 10:31:29 +0100

    Properties

---
+++ OEC222.Day1/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEC222.Day1
+{
+    public class Person
+    {
+        public string Name;  //Campo pubblico
+
+        public string Surname; //Campo pubblico
+
+
+        private DateOnly BirthDate;
+        public void SetBirthDate(DateOnly d)
+        {
+            BirthDate = d;
+        }
+
+        public DateOnly GetBirthDate()
+        {
+            return BirthDate;
+        }
+
+        private string email;
+
+        public string Email   //Proprietà pubblica su campo di appoggio privato
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+       
+    }
+}
--- OEC222.Day1/Program.cs
+++ OEC222.Day1/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Person p = new Person();
+
+            p.Name = "Davide";
+            p.Surname = "Maggiulli";
+
+            //p.BirthDate = new DateOnly(1987, 6, 27);
+            p.SetBirthDate(new DateOnly(1987,6,27));
+
+            Console.WriteLine("Nome: " + p.Name + " Cognome: " + p.Surname + " Data di nascita: " + p.GetBirthDate());
+
+            Console.ReadLine();
+
         }
     }
 }
